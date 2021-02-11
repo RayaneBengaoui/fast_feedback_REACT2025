df7c84f453a45c18038d23d0b5f61c274f3ab75d
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-02-11 11:41:42 +0100

    Adding Icons with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ import { Button, Heading, Text, Code } from "@chakra-ui/react";
 import { useAuth } from "@/lib/auth";
 import { auth } from "firebase";
 
+import { FastFeedbackIcon } from "../public/icon";
+
 const Home = () => {
   const auth = useAuth();
   return (
     <div>
       <main>
         <Heading>Fast Feedback </Heading>
-
+        <FastFeedbackIcon color="black" boxSize="32px" />
         <Text>
           Current user: <Code>{auth?.user ? auth.user.email : "None"}</Code>
         </Text>
