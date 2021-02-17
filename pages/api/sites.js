8cba3eb54043dbcb75d39baa6fd5f4e252ed214d
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-02-17 13:27:14 +0100

    Save feedback with a form

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -2,6 +2,10 @@ import { getAllSites } from "@/lib/db-admin";
 import db from "@/lib/firebase-admin";
 
 export default async function handler(_, res) {
-  const sites = await getAllSites();
+  const { sites, error } = await getAllSites();
+
+  if (error) {
+    res.status(500).json({ error });
+  }
   res.status(200).json({ sites });
 }
--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,10 +1,14 @@
 import Feedback from "@/components/Feedback";
+import { useAuth } from "@/lib/auth";
 import { getAllFeedback, getAllSites } from "@/lib/db-admin";
 import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+import { createFeedback } from "@/lib/db";
 
 export async function getStaticProps(context) {
   const siteId = context.params.siteId;
-  const feedback = await getAllFeedback(siteId);
+  const { feedback } = await getAllFeedback(siteId);
 
   return {
     props: {
@@ -14,7 +18,7 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const sites = await getAllSites();
+  const { sites } = await getAllSites();
   const paths = sites.map((site) => ({
     params: {
       siteId: site.id.toString(),
@@ -27,6 +31,26 @@ export async function getStaticPaths() {
 }
 
 const SiteFeedback = ({ initialFeedback }) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const inputEl = useRef(null);
+  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text: inputEl.current.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: "pending",
+    };
+    setAllFeedback([newFeedback, ...allFeedback]);
+    createFeedback(newFeedback);
+  };
   return (
     <Box
       display="flex"
@@ -35,15 +59,16 @@ const SiteFeedback = ({ initialFeedback }) => {
       maxWidth="700px"
       margin="0 auto"
     >
-      <FormControl my={8}>
-        <FormLabel>Comment</FormLabel>
-        <Input type="comment" id="comment" />
-        <Button mt={2} type="submit" fontWeight="medium">
-          Add Comment
-        </Button>
-      </FormControl>
-
-      {initialFeedback.map((feedback) => (
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl my={8}>
+          <FormLabel>Comment</FormLabel>
+          <Input ref={inputEl} type="comment" id="comment" />
+          <Button mt={2} type="submit" fontWeight="medium">
+            Add Comment
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback.map((feedback) => (
         <Feedback key={feedback.id} {...feedback} />
       ))}
     </Box>
