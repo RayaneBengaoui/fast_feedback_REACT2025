34e8f543d4c4c7c108a34b1c3f72f7ba1b7a199d
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-02-17 12:29:59 +0100

    Dynamic routing and getStaticProps / getStaticPaths

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,13 +1,7 @@
+import { getAllSites } from "@/lib/db-admin";
 import db from "@/lib/firebase-admin";
 
 export default async function handler(_, res) {
-  const snapshot = await db.collection("sites").get();
-  let sites = [];
-  snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc.data() });
-  });
-
+  const sites = await getAllSites();
   res.status(200).json({ sites });
-
-  //   res.status(200).json({ name: "John Doe" });
 }
---
+++ pages/p/[siteId].js
@@ -0,0 +1,34 @@
+import Feedback from "@/components/Feedback";
+import { getAllFeedback, getAllSites } from "@/lib/db-admin";
+
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const feedback = await getAllFeedback(siteId);
+
+  return {
+    props: {
+      initialFeedback: feedback,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const sites = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const SiteFeedback = ({ initialFeedback }) => {
+  return initialFeedback.map((feedback) => (
+    <Feedback key={feedback.id} {...feedback} />
+  ));
+};
+
+export default SiteFeedback;
